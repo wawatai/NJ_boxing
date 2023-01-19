dbfdd7112b69be624b4de50e6be75a4f7caa4570
wawatai <wataiPublic@gmail.com>
2023-01-19 18:27:11 +0800

    112-01-19

--- js/js.js
+++ js/js.js
@@ -57,6 +57,125 @@ function currentTime() {
     }
 }
 
+//mainArea
+$(function(){
+    //Swiper
+    var swiper = new Swiper('.swiper', {
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        slidesPerView: 1,
+        // spaceBetween: 30,
+        breakpoints: {
+            // 1600: {
+            //     slidesPerView: 1,
+            //     spaceBetween: 30,
+            // },
+            // 1260: {
+            //     slidesPerView: 1,
+            //     spaceBetween: 30,
+            // },
+            1000: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+    });
+    // var gtswiper = new Swiper('.gtswiper', {
+    //     autoHeight: true,
+    //     // autoplay: {
+    //     //     delay: 1500,
+    //     //     disableOnInteraction: false,
+    //     //     pauseOnMouseEnter: true,
+    //     // },
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     grid: {
+    //         fill: 'row',
+    //         rows: 2,
+    //     },
+    //     // breakpoints: {
+    //         // 1600: {
+    //         //     slidesPerView: 1,
+    //         //     spaceBetween: 30,
+    //         // },
+    //         // 1260: {
+    //         //     slidesPerView: 1,
+    //         //     spaceBetween: 30,
+    //         // },
+    //         // 1000: {
+    //         //     slidesPerView: 1,
+    //         //     spaceBetween: 0,
+    //         // }
+    //     // },
+    //     navigation: {
+    //         nextEl: '.gtswiper-button-next',
+    //         prevEl: '.gtswiper-button-prev',
+    //       },
+    //     // pagination: {
+    //     //   el: '.swiper-pagination',
+    //     //   clickable: true,
+    //     // },
+    // });
+ 
+    //countryList 國別一覽
+    $(".mainArea .countryList .cTitle").click(function(){
+        $(this).parent()
+        .toggleClass("active");
+
+        $(".mainArea .countryList ul")
+        .addClass("active");
+    })
+ 
+    //topnav
+    $(".topnav li").click(function(){
+        $(this)
+        .toggleClass("active")
+        .siblings()
+        .removeClass("active");
+    })
+ 
+    //gameTypeBox
+    $(".typselect .gameTypeBox").click(function(){
+        $(this)
+        .toggleClass("active")
+        .siblings()
+        .removeClass("active");
+    })
+ 
+    //ul smallTitle
+    $(".mainArea .countryList ul .smallTitle").click(function(){
+        $(this).parent()
+        .toggleClass("active");
+    })
+ 
+ 
+    $(document).on("click",".fa-regular.fa-star",function(){
+        $(this)
+        .attr("class","fa-solid fa-star");
+
+    })
+
+    $(document).on("click",".fa-solid.fa-star",function(){
+        $(this)
+        .attr("class","fa-regular fa-star");
+
+    })
+})
+
+
 //footer
 $(function(){
     $("footer .lang>.flag").click(function(){
